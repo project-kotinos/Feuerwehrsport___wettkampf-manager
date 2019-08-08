8d7ede618cc8b734f0d5e5524b1d35637961138c
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 19:55:18 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y tzdata xvfb
+apt-get update && apt-get install -y tzdata xvfb libfontconfig
+
+#install phantomjs
+rm -rf $PWD/travis_phantomjs; mkdir -p $PWD/travis_phantomjs
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs
+PATH=$PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64/bin:$PATH
+phantomjs --version
+
 gem install bundler -v 2.0.1
 # before_install
 sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules
